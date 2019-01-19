d802ce4c2bcb3a6d02c417f86e7b02d60ec34c96
jess3874 <jee3874@gmail.com>
2019-01-19 06:25:49 +0000

    Delete Homework_20150702(2).R

--- Homework_20150702(2).R
+++
@@ -1,9 +0,0 @@
-k=read.csv(file.choose())
-k
-dimnames(k)
-boxplot(k$value~k$region,col='bisque')
-title("Kids in districts of Seoul over ages")
-
-v2=tapply(k$value,k$age,sum)
-plot(c(0:6),v2,main="The number of Kids in each age",ylab="number",xlab="age")
-
